c0a3562e7431355b7f7996228359ff24b45adb00
Guido-87 <alvarezguido87@gmail.com>
2026-03-13 19:17:24 -0300

    Configuración Docker

--- ia/src/main/java/com/spring/ia/IaApplication.java
+++ ia/src/main/java/com/spring/ia/IaApplication.java
@@ -2,8 +2,15 @@ package com.spring.ia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				DataSourceAutoConfiguration.class,
+				HibernateJpaAutoConfiguration.class
+		}
+)
 public class IaApplication {
 
 	public static void main(String[] args) {
